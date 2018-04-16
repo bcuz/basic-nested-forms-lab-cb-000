27f71b359e515fd96804c6dadb44ce18e9a3ed4e
adam n <adam.nawroc@gmail.com>
2018-04-16 12:01:27 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -27,7 +27,8 @@ class RecipesController < ApplicationController
 
   def recipe_params
     params.require(:recipe).permit(:title, ingredients_attributes: [
-      :
+      :name,
+      :quantity
       ])
   end
 end
